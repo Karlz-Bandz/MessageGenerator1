c3b377cdee3e70cb232308f5ce8393a1f85620b3
Melak <km250542@ncr.com>
2023-05-06 10:28:23 +0200

    LocalStorage functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HR-Helper';
 
-  boxNumber: number = 37;
+  boxNumber: number = 0;
 
-  boxArray = Array(this.boxNumber).fill(0);
+  //boxArray = Array(this.boxNumber).fill(0);
+
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    const storedMessagesString = localStorage.getItem("messages");
+
+    if(storedMessagesString != null)
+    {
+      this.messages = JSON.parse(storedMessagesString);
+    }
+
+
+  }
+
+  addNewTextBox(): void{
+
+    this.messages.push("kk");
+    localStorage.setItem("messages", JSON.stringify(this.messages));
+  }
+
+  removeTextBox(index: number): void{
+    this.messages.splice(index, 1);
+    localStorage.setItem("messages", JSON.stringify(this.messages));
+
+  }
 
 
 }
